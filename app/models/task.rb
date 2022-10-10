2da1c1924ed680c2335e0f8ecff0a5aa16758d0f
aynur <aykuli@ya.ru>
2022-10-10 15:18:17 +0700

    task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
-  belongs_to :author, class_name: "User"
-  belongs_to :assignee, class_name: "User", optional: true
+  belongs_to :author, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
 
   validates :name, :description, :author, presence: true
-  validates :description, length: {maximum: 500 }
+  validates :description, length: { maximum: 500 }
 end
