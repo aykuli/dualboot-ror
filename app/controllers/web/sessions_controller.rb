3c453076f20262eadce008f126183745a62b1bd5
aynur <aykuli@ya.ru>
2022-10-12 15:52:00 +0700

    session controller defs

--- app/controllers/web/sessions_controller.rb
+++ app/controllers/web/sessions_controller.rb
@@ -1,3 +1,28 @@
 class Web::SessionsController < Web::ApplicationController
-  def new; end
+  def new
+    @session = SessionForm.new
+  end
+
+  def create
+    puts("create params: #{params}")
+    @session = SessionForm.new(session_params)
+
+    if @session.valid?
+      sign_in(@session.user)
+      redirect_to(:board)
+    else
+      render(:new)
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to(:new_session)
+  end
+
+  private
+
+  def session_params
+    params.require(:session_form).permit(:email, :password)
+  end
 end
