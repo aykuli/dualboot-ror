9549af77ae8bfbdcc24777fed1a0fa4a79fcaa0a
aynur <aykuli@ya.ru>
2022-10-17 21:15:53 +0700

    rubocop

--- app/controllers/concerns/auth_helper.rb
+++ app/controllers/concerns/auth_helper.rb
@@ -1,16 +1,23 @@
 module AuthHelper
-  def sign_in(user) = session[:user_id] = user.id
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
 
-  def sign_out = session[:user_id] = nil
+  def sign_out
+    session[:user_id] = nil
+  end
 
-  def signed_in? = current_user.present?
+  def signed_in?
+    current_user.present?
+  end
 
   def authenticate_user!
-    redirect_to new_session_path unless signed_in?
+    redirect_to(new_session_path) unless signed_in?
   end
 
   def current_user
     return if session[:user_id].blank?
-    @_current_user ||= User.find_by(id: session[:user_id])
+
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 end
