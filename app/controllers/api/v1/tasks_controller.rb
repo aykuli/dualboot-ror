142ce6535875ec3435a9fd02820f214c7a237bb6
aynur <aykuli@ya.ru>
2022-10-28 20:02:15 +0700

    task show and create

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -5,4 +5,22 @@ class Api::V1::TasksController < Api::ApplicationController
 
     respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
   end
+
+  def show
+    task = Task.find(params[:id])
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def create
+    task = current_user.my_tasks.new(task_params)
+    task.save
+
+    respond_with(task, serializer: TaskSerializer, location: nil)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :author_id, :assignee_id, :state_event)
+  end
 end
