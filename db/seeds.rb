b6da710eb281f92a17f91cbd4ade8980c314b6ad
aynur <aykuli@ya.ru>
2022-10-30 23:46:47 +0700

    refactor seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,35 @@
-Dir[File.join(Rails.root, "db", "seeds", "*.rb")].sort.each do |seed|
-  load seed
+admin = Admin.find_or_create_by(first_name: 'Aynur', last_name: 'Shauerman', email: 'admin@localhost')
+admin.password = '123456789'
+admin.save
+
+60.times do |i|
+  u = [Manager, Developer].sample.new
+  u.email = "email#{i}@example.com"
+  u.first_name = "Firstname#{i}"
+  u.last_name = "Lastname#{i}"
+  u.password = i.to_s
+  u.save
 end
+
+manager = Manager.first
+developer = Developer.first
+developer_second = Developer.second
+
+10.times do |i|
+  task = manager.my_tasks.new(
+    name: "Task #{i}",
+    description: "Task description #{i}",
+    assignee_id: developer.id,
+    )
+  task.save
+end
+
+5.times do |i|
+  task = manager.my_tasks.new(
+    name: "Task 1 #{i}",
+    description: "Task description 1 #{i}",
+    assignee: developer_second,
+    )
+  task.save
+end
+
