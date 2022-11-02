721eb973cb7d4f9d183f67442d09098bfc7122be
aynur <aykuli@ya.ru>
2022-11-02 16:15:29 +0700

    ecthHelper and keyConverter

--- utils/keysConverter.js
+++ utils/keysConverter.js
@@ -1,4 +1,4 @@
 import humps from 'humps';
 
-export const decamelize = (obj) => humps.decamelizeKeys(obj);
+export const decamelize = (obj, options = {}) => humps.decamelizeKeys(obj, options);
 export const camelize = (obj, options = {}) => humps.camelizeKeys(obj, options);
