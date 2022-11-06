2bae4b31b4e98dd7df34cec45e51c69c780257cc
aynur <aykuli@ya.ru>
2022-11-06 21:45:11 +0700

    users api v1

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::UsersController < Api::V1::ApplicationController
+  def index
+    users = User.ransack(ransack_params).result.page(page).per(per_page)
+
+    respond_with( json: users, each_serializer: UserSerializer, root: 'items', meta: build_meta(users))
+  end
+
+  def show
+    user = User.find(params[:id])
+
+    respond_with(json: user, serializer: UserSerializer)
+  end
+end
