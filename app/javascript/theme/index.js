c1956cd87991c64c17d1bbe9be075378a1784553
aynur <aykuli@ya.ru>
2022-11-10 18:46:01 +0700

    theming brown yellow

--- app/javascript/theme/index.js
+++ app/javascript/theme/index.js
@@ -3,10 +3,10 @@ import { createTheme } from '@material-ui/core/styles';
 export default createTheme({
   palette: {
     primary: {
-      main: '#ffee58',
+      main: '#795548',
     },
     secondary: {
-      main: '#66bb6a',
+      main: '#ffab40',
     },
   },
 });
