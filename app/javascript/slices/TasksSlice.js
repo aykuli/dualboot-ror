e4adb3c8807ab5e306deb36d59609b5b3e93412e
aynur <aykuli@ya.ru>
2022-11-15 19:34:53 +0700

    fix 1

--- app/javascript/slices/TasksSlice.js
+++ app/javascript/slices/TasksSlice.js
@@ -1,8 +1,10 @@
 import { propEq } from 'ramda';
 import { createSlice } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 import { changeColumn } from '@asseinfo/react-kanban';
 
-import { initialColumns, MODE } from 'constants/board';
+import TasksRepository from 'repositories/TasksRepository';
+import { initialColumns, MODE, STATE } from 'constants/board';
 import { SEVERITY } from 'constants/ui';
 
 const initialState = {
@@ -18,7 +20,6 @@ const initialState = {
       type: SEVERITY.ERROR,
     },
     mode: MODE.NONE,
-    errors: null,
   },
 };
 
@@ -55,7 +56,94 @@ const tasksSlice = createSlice({
   },
 });
 
-export const { loadColumnSuccess, showSnackbar, changeModalState, setEditingTaskId, setCurrentTask, setFormErrors } =
-  tasksSlice.actions;
+const { loadColumnSuccess, showSnackbar, changeModalState, setEditingTaskId, setCurrentTask } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
+
+export const useTasksActions = () => {
+  const dispatch = useDispatch();
+
+  const loadColumn = (state, page = 1, perPage = 10) => {
+    TasksRepository.index({
+      q: { stateEq: state },
+      page,
+      perPage,
+    }).then(({ data }) => {
+      dispatch(loadColumnSuccess({ ...data, columnId: state }));
+    });
+  };
+
+  const createTask = (attributes) =>
+    TasksRepository.create(attributes)
+      .then(() => {
+        loadColumn(STATE.NEW_TASK);
+        dispatch(showSnackbar({ type: SEVERITY.SUCCESS, text: 'Task created and saved!' }));
+
+        dispatch(changeModalState(MODE.NONE));
+      })
+      .catch((error) => {
+        dispatch(showSnackbar({ type: SEVERITY.ERROR, text: `Task creating failed! ${error?.message || ''}` }));
+      });
+
+  const updateTaskForDragAndDrop = (sourceColumnId, destinationColumnId) => {
+    loadColumn(sourceColumnId);
+    loadColumn(destinationColumnId);
+  };
+
+  const updateTask = (task, attributes) =>
+    TasksRepository.update(task.id, attributes)
+      .then(() => {
+        loadColumn(task.state);
+        dispatch(showSnackbar({ type: SEVERITY.SUCCESS, text: 'Task was updated!' }));
+
+        dispatch(changeModalState(MODE.NONE));
+      })
+      .catch((error) => {
+        dispatch(showSnackbar({ type: SEVERITY.ERROR, text: `Task updating failed! Error: ${error?.message || ''}` }));
+      });
+
+  const destroyTask = (task) =>
+    TasksRepository.destroy(task.id)
+      .then(() => {
+        loadColumn(task.state);
+        dispatch(showSnackbar({ type: SEVERITY.SUCCESS, text: 'Task was destroyed!' }));
+
+        dispatch(changeModalState(MODE.NONE));
+      })
+      .catch((error) => {
+        dispatch(
+          showSnackbar({ type: SEVERITY.ERROR, text: `Task destroying was failed! Error: ${error?.message || ''}` }),
+        );
+      });
+
+  const setEditingTask = (id) => {
+    dispatch(setEditingTaskId(id));
+    dispatch(changeModalState(MODE.EDIT));
+  };
+
+  const loadTask = (taskId) => {
+    dispatch(changeModalState(MODE.EDIT));
+
+    TasksRepository.show(taskId)
+      .then(({ data: { task } }) => dispatch(setCurrentTask(task)))
+      .catch((error) => {
+        dispatch(
+          showSnackbar({
+            type: SEVERITY.ERROR,
+            text: `Load task failed! Please, try again. Error: ${error?.message || ''}`,
+          }),
+        );
+        dispatch(changeModalState(MODE.NONE));
+      });
+  };
+
+  return { loadColumn, loadTask, createTask, updateTask, destroyTask, setEditingTask, updateTaskForDragAndDrop };
+};
+
+export const useUiAction = () => {
+  const dispatch = useDispatch();
+
+  const setMode = (mode) => dispatch(changeModalState(mode));
+
+  return { setMode };
+};
