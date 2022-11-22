e9dd7c853bccd15bac76703b94fc38db1254b136
aynur <aykuli@ya.ru>
2022-11-22 23:58:17 +0700

    seeds

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,7 +7,7 @@ EDITOR="mate --wait" bin/rails credentials:edit
 
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rake db:migrate db:seed
 cp config/webpacker.example.yml config/webpacker.yml
 cp babel.config.example.js babel.config.js
 
