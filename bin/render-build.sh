801c5a9f51ca56f8e2e0817143151b3632f6140f
aynur <aykuli@ya.ru>
2022-11-22 17:27:16 +0700

    set editor for credentials edit

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,11 @@
 set -o errexit
 
 bundle install
+EDITOR="mate --wait" bin/rails credentials:edit
+bundle exec rails webpacker:install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
 cp config/webpacker.example.yml config/webpacker.yml
 cp babel.config.example.js babel.config.js
+
