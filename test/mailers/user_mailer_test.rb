defb47786bc2012edcc5b71e15fea1f67d16d5d2
aynur <aykuli@ya.ru>
2022-11-27 16:54:51 +0700

    mailer test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -48,4 +48,21 @@ class UserMailerTest < ActionMailer::TestCase
     assert_equal 'Task was destroyed', email.subject
     assert email.body.to_s.include?("Task &#39;#{task.name}&#39; was destroyed")
   end
+
+  test 'password reset' do
+    user = create(:user)
+    user.reset_token = User.new_token
+    params = { user: user }
+
+    email = UserMailer.with(params).password_reset
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal ['noreply@taskmanager.com'], email.from
+    assert_equal "Password reset", email.subject
+    assert_equal [user.email], eail.to
+    assert_match user.reset_token, email.body.encoded
+  end
 end
