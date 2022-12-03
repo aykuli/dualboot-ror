876ea8f67cc1398c52cbe6cea04156da809d0292
aynur <aykuli@ya.ru>
2022-12-03 20:42:34 +0700

    sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -11,3 +11,4 @@ bundle exec rake db:migrate db:seed
 cp config/webpacker.example.yml config/webpacker.yml
 cp babel.config.example.js babel.config.js
 
+curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY=$NRIA_LICENSE_KEY NEW_RELIC_ACCOUNT_ID=$NEW_RELIC_ACCOUNT_ID /usr/local/bin/newrelic install -n logs-integration
